6f444d5cb307fc79beff4e6fb566df9054dd5fbe
mykevinjj <h849179@gmail.com>
2023-07-24 23:33:49 +0800

    0.2

--- main.js
+++ main.js
@@ -3,28 +3,35 @@ import OSM from './ol/source/OSM.js';
 import TileLayer from './ol/layer/Tile.js';
 import View from './ol/View.js';
 import * as proj from './ol/proj.js'
-alert("非正式版")
-var sc1_1 = document.getElementById('sc1')
-
-var ol = document.getElementById('o1')
 var lat =25.033671
 var long = 121.564427
-var map = new Map({
+alert("非正式版")  
+function view_a() 
+{
+  var h1 = document.getElementById('h12')
+  sc1_1.style.visibility ='visible'
+  h1.innerText=""
+  ol.innerText="請選擇校車路線"
+}
+var sc1_1 = document.getElementById('sc1')
+var ol = document.getElementById('o1')
+var map = new Map
+({
   target: 'map',
-  layers: [
-    new TileLayer({
+  layers:
+  [
+    new TileLayer
+    ({
       source: new OSM(),
     }),
   ],
-  view: new View({
+  view: new View
+  ({
     center:proj.fromLonLat([long,lat]),
     zoom: 10,
   }),
 });
-var h1 = document.getElementById('h12')
-sc1_1.style.visibility ='visible'
-h1.innerText=""
-ol.innerText="請選擇校車路線"
+view_a()
 function link (long_in,lat_in,zoom_i){
   var long_o = long_in 
   var lat_o = lat_in
@@ -33,17 +40,22 @@ function link (long_in,lat_in,zoom_i){
   map.getView().setZoom(zoom_o)
   map.render()
 }
-sc1_1.onchange = function(){
+sc1_1.onchange = function()
+{
   let sc1 = document.getElementById('sc1').value
-  if (sc1=='0') {
-       link(121.564427,25.033671,10)
+  if (sc1=='0')
+  {
+  
+    link(long,lat,10)
+  }
+  else
+  {
+    var zoom= 19
+    if (sc1=='義民中學')
+    {
+      var lat_s = 24.83871439290991
+      var long_s =  121.00394288581761
+      link(long_s,lat_s,zoom)
     }
-    else{
-      var zoom= 18.5
-      if (sc1=='義民中學') {
-          var lat_s = 24.83916811708179
-          var long_s = 121.0029220390707
-          link(long_s,lat_s,zoom)
-      }
-    }
+  }
 }
