1eecb5e81d85ad8aec08f240f0b974cc449dcf4d
kevinjj <98082057+ke-wordaa@users.noreply.github.com>
2023-12-08 11:47:37 +0800

    a

--- main.js
+++ main.js
@@ -1,26 +1,5 @@
-var sc1 = document.getElementById('sc1')
-var sc2 = document.getElementById('sc2')
+var map = L.map('map').setView([24.838298538785217, 121.00369361036967], 18);
 
-async function myFunction() {
-    const response = await fetch("main.json")
-    const data = await response.json()
-    let routes = data.routename      
-    let classa = data.Classification    
-    for (let  i = 0; i < classa.length; i++) 
-    {
-        sc1.innerHTML +="<option>"+classa[i]+"</option>"
-    }    
-    sc1.addEventListener('change',()=>{
-        if (sc2.length!=1) {
-            sc2.innerHTML = '<option></option>'
-        }
-        for (let i = 0; i < routes.length; i++) 
-        {
-            for (let j = 0; j < routes[i][sc1.value].length; j++) {
-                sc2.innerHTML +="<option>"+ routes[i][sc1.value][j]+"</option>"
-            }
-       }
-   })
-}
-
-  
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
