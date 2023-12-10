69cb80fcb28ae49e7dc8ba4531030bbbdcb17aa1
kevinjj <98082057+ke-wordaa@users.noreply.github.com>
2023-12-10 22:26:56 +0800

    a

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-var school = [24.838298538785217, 121.00369361036967]
-var map = L.map('map').setView(school,18);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '義民高中校車路線查詢器'
-}).addTo(map);
+$(document).ready(function () {
+    $('#win').modal('show');
+});
