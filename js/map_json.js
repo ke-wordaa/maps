e39f82833711ee669eb95b66d7abced86286e16e
kevinjj <98082057+ke-wordaa@users.noreply.github.com>
2023-12-11 23:12:42 +0800

    112_12_11

--- js/map_json.js
+++ js/map_json.js
@@ -1,10 +1,39 @@
-async function map_json(){
+$(window).ready(function () {
     var school = [24.838298538785217, 121.00369361036967]
     var map = L.map('map').setView(school,18);
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '義民高中校車路線查詢器'
-    }).addTo(map);
-}
-$(document).ready(function () {
-    map_json()
-});
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '義民高中校車路線查詢器'
+        }).addTo(map);
+$.ajax
+({
+    url: "../json/main.json",
+    success: function(data) 
+    {
+        for (let i = 0; i < data.Classification.length; i++) 
+        {
+            $("#sc1").append("<option>"+data.Classification[i]+"</option>");
+        }
+        $("#sc1").change(function (e) 
+        {
+            let sc2 = $("#sc2").children()
+            let sc1_v = $("#sc1").val();
+            if(sc2.length!=1)
+            {
+                $("#sc2").empty();;
+                $("#sc2").html('<option></option>');;
+            }
+            if (sc1_v) 
+            {  
+                for (let i = 0;i <data.routename[sc1_v].length; i++) 
+                {
+                    $('#sc2').append("<option>"+data.routename[sc1_v][i]+"</option>");
+                }
+            }
+            else
+            {
+                $("#sc2").empty();;
+                $("#sc2").html('<option></option>');;
+            }
+        })
+    }})
+})
