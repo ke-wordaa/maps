e26b991812aa144dd38962d275daa05620beaaa7
kevinjj <98082057+ke-wordaa@users.noreply.github.com>
2023-12-15 11:40:31 +0800

    AS

--- js/map_json.js
+++ js/map_json.js
@@ -4,7 +4,7 @@ $(window).ready(function () {
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', 
     {
             attribution: '義民高中校車路線查詢器'
-    }).addTo(map);
+    }).addTo(map)
 $.ajax  
 ({
     url: "json/main.json",
@@ -12,34 +12,35 @@ $.ajax
     {
         for (let i = 0; i < data.Classification.length; i++) 
         {
-            $("#sc1").append("<option>"+data.Classification[i]+"</option>");
+            $("#sc1").append("<option>"+data.Classification[i]+"</option>")
         }
         $("#sc1").change(function () 
         {
             let sc2 = $("#sc2").children()
-            let sc1_v = $("#sc1").val();
+            let sc1_v = $("#sc1").val()
             if(sc2.length!=1)
             {
-                $("#sc2").empty();;
-                $("#sc2").html('<option></option>');;
+                $("#sc2").empty()
+                $("#sc2").html('<option></option>')
             }
             if (sc1_v) 
             {  
                 for (let i = 0;i <data.routename[sc1_v].length; i++) 
                 {
-                    $('#sc2').append("<option>"+data.routename[sc1_v][i]+"</option>");
+                    $('#sc2').append("<option>"+data.routename[sc1_v][i]+"</option>")
                 }
             }
             else
             {
-                $("#sc2").empty();;
-                $("#sc2").html('<option></option>');;
+                $("#sc2").empty()
+                $("#sc2").html('<option></option>')
             }
         })
         $("#sc2").change(function () { 
             let a =""
             let sc1_v = $("#sc1").val()
             let sc2_v = $("#sc2").val()
+
             if (sc2_v) {  
                 for (let i = 0; i < data.coordinate[sc1_v][sc2_v].length; i++) {
                     a += data.coordinate[sc1_v][sc2_v][i][0]+"\n"
